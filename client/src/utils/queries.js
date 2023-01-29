5416d35424d7b655222c7c6a4febec22ca5a04f1
Miles Thomas <milesman2013@gmail.com>
2023-01-29 17:27:45 +1100

    theory front-end queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -7,5 +7,63 @@ query me {
         name
         notes
     }
-}
-`
+}`
+
+export const QUERY_CHORDS = gql`
+query chords {
+    chords {
+        _id
+        name
+        structure
+        description
+        image
+        subsection
+    }
+}`
+
+export const QUERY_INTERVALS = gql`
+query intervals {
+    intervals {
+        _id
+        name
+        description
+        image
+        referenceSongs
+        subsection
+    }
+}`
+
+export const QUERY_RHYTHMS = gql`
+query rhythms {
+    rhythms {
+        _id
+        name
+        description
+        image
+        subsection
+    }
+}`
+
+export const QUERY_SCALES = gql`
+query scales {
+    scales {
+        _id
+        name
+        structure
+        description
+        image
+        subsection
+    }
+}`
+
+export const QUERY_SYMBOLS = gql`
+query symbols {
+    symbols {
+        _id
+        name
+        description
+        image
+        subsection
+    }
+}`
+
