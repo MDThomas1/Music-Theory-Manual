db692cb79296b38de8b1e51cdb48d0fcfaa701d8
Miles Thomas <milesman2013@gmail.com>
2023-01-29 16:10:35 +1100

    key sig schema

--- server/models/Key-Signature.js
+++ server/models/Key-Signature.js
@@ -1,7 +1,18 @@
 const { Schema, model } = require('mongoose');
 
 const keySigSchema = new Schema({
-    
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
 })
 
 const KeySignature = model('KeySignature', keySigSchema)
