c9069166e4e855a79c1ddc0b2ef2d2be3aea03a2
Miles Thomas <milesman2013@gmail.com>
2023-01-29 16:09:37 +1100

    interval schema

--- server/models/Interval.js
+++ server/models/Interval.js
@@ -1,7 +1,23 @@
 const { Schema, model } = require('mongoose');
 
 const intervalSchema = new Schema({
-
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    referenceSongs: [String],
+    subsection: {
+        type: String,
+        required: true
+    }
 })
 
 const Interval = model('Interval', intervalSchema)
