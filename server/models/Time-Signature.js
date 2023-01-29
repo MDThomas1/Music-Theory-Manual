953b6e2d597c2917c837e12f51df9b3d968d3315
Miles Thomas <milesman2013@gmail.com>
2023-01-29 16:13:05 +1100

    time sig schema

--- server/models/Time-Signature.js
+++ server/models/Time-Signature.js
@@ -1,7 +1,18 @@
 const { Schema, model } = require('mongoose');
 
 const timeSigSchema = new Schema({
-
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    }
 })
 
 const TimeSignature = model('TimeSignature', timeSigSchema)
