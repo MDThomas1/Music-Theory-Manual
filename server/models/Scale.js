69e3e5e5fa47894c0ea147922e6c1745912b4e90
Miles Thomas <milesman2013@gmail.com>
2023-01-29 16:12:17 +1100

    scale schema

--- server/models/Scale.js
+++ server/models/Scale.js
@@ -1,7 +1,26 @@
 const { Schema, model } = require('mongoose');
 
 const scaleSchema = new Schema({
-
+    name: {
+        type: String,
+        required: true
+    },
+    structure: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    subsection: {
+        type: String,
+        required: true
+    }
 })
 
 const Scale = model('Scale', scaleSchema)
