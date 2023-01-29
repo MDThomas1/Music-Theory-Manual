26fa0d3050045b4191b22b474b49d35c0b3d29d5
Miles Thomas <milesman2013@gmail.com>
2023-01-29 16:05:03 +1100

    chord schema

--- server/models/Chord.js
+++ server/models/Chord.js
@@ -1,7 +1,26 @@
 const { Schema, model } = require('mongoose');
 
 const chordSchema = new Schema({
-
+    name: {
+        type: String,
+        required: true
+    },
+    structure: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    subsection: {
+        type: String,
+        required: true
+    }
 })
 
 const Chord = model('Chord', chordSchema)
