4930ad0e3ae133a3ace3e97ec30efda136a48c60
Miles Thomas <milesman2013@gmail.com>
2023-01-30 18:01:05 +1100

    login form revision

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -42,8 +42,8 @@ const Login = (props) => {
             <h3>Login here to get started!</h3>
             <div>
                 <form onSubmit={handleFormSubmit}>
-                    <input placeholder='Enter your email here' name='email' type={email} value={formState.email} onChange={handleChange} />
-                    <input placeholder='Enter your password here' name='password' type={password} value={formState.password} onChange={handleChange} />
+                    <input placeholder='Enter your email here' name='email' type='email' value={formState.email} onChange={handleChange} />
+                    <input placeholder='Enter your password here' name='password' type='password' value={formState.password} onChange={handleChange} />
                     <button type='submit'>Submit</button>
                 </form>
             </div>
