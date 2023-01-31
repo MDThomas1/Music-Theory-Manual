a6a67b88db7deb882868438e5c8ba3728c63aaeb
Miles Thomas <milesman2013@gmail.com>
2023-01-31 19:07:03 +1100

    added form for making notes

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { useQuery, useMutation } from '@apollo/client';
 
@@ -9,16 +9,34 @@ import { CREATE_NOTE, DELETE_NOTE } from '../utils/mutations'
 const Profile = () => {
     const { data } = useQuery(QUERY_ME)
 
-    const [createNote, { error }] = useMutation(CREATE_NOTE)
-
     const profile = data
+
+    const [note, setNote] = useState('')
+
+    const [createNote, { error }] = useMutation(CREATE_NOTE)
+ 
+    const handleFormSubmit = async (event) => {
+        event.preventDefault()
+
+        try {
+            const data = await createNote({
+                variables: { note }
+            })
+
+            setNote('')
+
+        } catch (error) {
+            console.error(error)
+        }
+    }
 
     return (
         <div>
             <h3>Welcome {profile.name}</h3>
             <div>
-                <form>
-                    <input />
+                <form onSubmit={handleFormSubmit}>
+                    <input placeholder='Create a new note!' value={note} onChange={(event) => setNote(event.target.value)} />
+                    <button type='submit'>Create</button>
                 </form>
             </div>
         </div>
