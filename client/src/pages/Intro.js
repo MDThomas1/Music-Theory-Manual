95efe23561b4271a83c4fb9f8d2fc6345c54a765
Miles Thomas <milesman2013@gmail.com>
2023-01-31 20:48:13 +1100

    beginning intro page content

--- client/src/pages/Intro.js
+++ client/src/pages/Intro.js
@@ -4,7 +4,10 @@ import { Link } from 'react-router-dom';
 import Auth from '../../utils/auth';
 
 const Intro = () => {
-    
+    <div id='introduction'>
+        <h3>Introduction to Music Theory</h3>
+        <p></p>
+    </div>
 }
 
 export default Intro
