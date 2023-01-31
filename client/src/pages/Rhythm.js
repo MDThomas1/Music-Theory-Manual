4503c21868ec3804401125f7b6e7f234f0beae80
Miles Thomas <milesman2013@gmail.com>
2023-01-31 20:40:10 +1100

    rhythm page organised

--- client/src/pages/Rhythm.js
+++ client/src/pages/Rhythm.js
@@ -4,7 +4,24 @@ import { Link } from 'react-router-dom';
 import Auth from '../../utils/auth';
 
 const Rhythm = () => {
-    
+    return (
+        <div id='rhythm'>
+            <h3>Rhythm</h3>
+            <p></p>
+            <div id='common-rhythms'>
+                <h4>Common Rhythms</h4>
+                <p></p>
+            </div>
+            <div id='rests'>
+                <h4>Rests</h4>
+                <p></p>
+            </div>
+            <div id='triplets'>
+                <h4>Triplets</h4>
+                <p></p>
+            </div>
+        </div>
+    )
 }
 
-export default Navbar
+export default Rhythm
