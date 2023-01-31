4ad94e7bdc3afb494eeaa2e118dc3010b71dec67
Miles Thomas <milesman2013@gmail.com>
2023-01-31 18:45:38 +1100

    profile form beginning

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,11 +1,28 @@
 import React from 'react';
 
-import { useParams } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
+import { useQuery, useMutation } from '@apollo/client';
 
 import { QUERY_ME } from '../utils/queries';
 
+import { CREATE_NOTE, DELETE_NOTE } from '../utils/mutations'
+
 const Profile = () => {
+    const { data } = useQuery(QUERY_ME)
+
+    const [createNote, { error }] = useMutation(CREATE_NOTE)
+
+    const profile = data
+
+    return (
+        <div>
+            <h3>Welcome {profile.name}</h3>
+            <div>
+                <form>
+                    <input />
+                </form>
+            </div>
+        </div>
+    )
 
 }
 
