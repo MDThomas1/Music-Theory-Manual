23e1f58bd827d8dc93685b9e6a983d673621b802
Miles Thomas <milesman2013@gmail.com>
2023-01-31 19:25:59 +1100

    Note list

---
+++ client/src/components/NoteList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { useMutation } from '@apollo/client';
+
+import { DELETE_NOTE } from '../../utils/mutations';
+import { QUERY_ME } from '../../utils/queries';
+
+const NoteList = () => {
+
+}
+
+export default NoteList
