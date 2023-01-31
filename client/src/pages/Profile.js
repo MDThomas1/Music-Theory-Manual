6e66eef66e301fd4dfce884ca9b4da264b36f2c9
Miles Thomas <milesman2013@gmail.com>
2023-01-31 19:20:24 +1100

    conditional for notes list

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -34,10 +34,22 @@ const Profile = () => {
         <div>
             <h3>Welcome {profile.name}</h3>
             <div>
+                <h4>Create a new note</h4>
                 <form onSubmit={handleFormSubmit}>
-                    <input placeholder='Create a new note!' value={note} onChange={(event) => setNote(event.target.value)} />
+                    <input placeholder='Enter new note here' value={note} onChange={(event) => setNote(event.target.value)} />
                     <button type='submit'>Create</button>
                 </form>
+            </div>
+            <div>
+                <h4>Your Notes List</h4>
+                {!profile.notes.length ? 
+                <p>
+                    You don't currently have any notes. Use the form above to create some.
+                </p>
+                : 
+                <div>
+                    
+                </div> }
             </div>
         </div>
     )
