77dda7962ab01507dd3fcbb332598ee9a70c6d6b
Miles Thomas <milesman2013@gmail.com>
2023-01-31 20:45:28 +1100

    scales page organised

--- client/src/pages/Scales.js
+++ client/src/pages/Scales.js
@@ -4,7 +4,24 @@ import { Link } from 'react-router-dom';
 import Auth from '../../utils/auth';
 
 const Scales = () => {
-    
+    return (
+        <div id='scales'>
+            <h3>Scales</h3>
+            <p></p>
+            <div id='major-modes'>
+                <h4>Major Modes</h4>
+                <p></p>
+            </div>
+            <div id='pentatonic-scales'>
+                <h4>Pentatonic Scales</h4>
+                <p></p>
+            </div>
+            <div id='useful-scales'>
+                <h4>Other Useful Scales</h4>
+                <p></p>
+            </div>
+        </div>
+    )
 }
 
 export default Scales
