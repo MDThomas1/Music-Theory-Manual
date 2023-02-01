13364a5fa4a8fa5b38eb6ccd7c237e65877e9315
Miles Thomas <milesman2013@gmail.com>
2023-02-02 01:09:49 +1100

    made routes rather than renderPage

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Header from './components/Header';
 import Navbar from './components/Navbar';
@@ -86,11 +87,29 @@ function App() {
 
     return (
         <ApolloProvider client={client}>
-            <Header currentPage={currentPage} handlePageChange={handlePageChange}/>
-            <Navbar currentPage={currentPage} handlePageChange={handlePageChange}/>
-            {renderPage()}
-            <Footer />
-
+            <Router>
+                <div>
+                    <Header />
+                    <Navbar />
+                </div>
+                <div>
+                    <Routes>
+                        <Route path='/' element={<Home />} />
+                        <Route path='/login' element={<Login />} />
+                        <Route path='/profile' element={<Profile />} />
+                        <Route path='/signup' element={<Signup />} />
+                        <Route path='/chords' element={<Chords />} />
+                        <Route path='/intervals' element={<Intervals />} />
+                        <Route path='/intro' element={<Intro />} />
+                        <Route path='/notation' element={<Notation />} />
+                        <Route path='/rhythm' element={<Rhythm />} />
+                        <Route path='/scales' element={<Scales />} />
+                    </Routes>
+                </div>
+                <div>
+                    <Footer />
+                </div>
+            </Router>
         </ApolloProvider>
     )
 }
