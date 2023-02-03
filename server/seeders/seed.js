232f5b825764431569ee04bcbee7e33e09baed3e
Miles Thomas <milesman2013@gmail.com>
2023-02-03 17:44:55 +1100

    cleaned up seeder file

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -25,61 +25,61 @@ const userSeeds = require('./userSeeds.json');
 const rhythmSeeds = require('./rhythmSeeds.json');
 
 db.once('open', async () => {
-  try {
-    await User.deleteMany({});
-    await User.create(userSeeds);
+    try {
+        await User.deleteMany({});
+        await User.create(userSeeds);
 
-    await Clef.deleteMany({});
-    await Clef.create();
+        await Clef.deleteMany({});
+        await Clef.create();
 
-    await Accidental.deleteMany({});
-    await Accidental.create();
+        await Accidental.deleteMany({});
+        await Accidental.create();
 
-    await KeySignature.deleteMany({});
-    await KeySignature.create();
+        await KeySignature.deleteMany({});
+        await KeySignature.create();
 
-    await Rhythm.deleteMany({});
-    await Rhythm.create(rhythmSeeds);
+        await Rhythm.deleteMany({});
+        await Rhythm.create(rhythmSeeds);
 
-    await Rest.deleteMany({});
-    await Rest.create();
+        await Rest.deleteMany({});
+        await Rest.create();
 
-    await Triplet.deleteMany({});
-    await Triplet.create();
+        await Triplet.deleteMany({});
+        await Triplet.create();
 
-    await PerfectInterval.deleteMany({});
-    await PerfectInterval.create();
+        await PerfectInterval.deleteMany({});
+        await PerfectInterval.create();
 
-    await MajorInterval.deleteMany({});
-    await MajorInterval.create();
+        await MajorInterval.deleteMany({});
+        await MajorInterval.create();
 
-    await MinorInterval.deleteMany({});
-    await MinorInterval.create();
+        await MinorInterval.deleteMany({});
+        await MinorInterval.create();
 
-    await MajorMode.deleteMany({});
-    await MajorMode.create();
+        await MajorMode.deleteMany({});
+        await MajorMode.create();
 
-    await Pentatonic.deleteMany({});
-    await Pentatonic.create();
+        await Pentatonic.deleteMany({});
+        await Pentatonic.create();
 
-    await OtherScale.deleteMany({});
-    await OtherScale.create();
+        await OtherScale.deleteMany({});
+        await OtherScale.create();
 
-    await Triad.deleteMany({});
-    await Triad.create();
+        await Triad.deleteMany({});
+        await Triad.create();
 
-    await SusChord.deleteMany({});
-    await SusChord.create();
+        await SusChord.deleteMany({});
+        await SusChord.create();
 
-    await SixthChord.deleteMany({});
-    await SixthChord.create();
+        await SixthChord.deleteMany({});
+        await SixthChord.create();
 
-    await SeventhChord.deleteMany({});
-    await SeventhChord.create();
+        await SeventhChord.deleteMany({});
+        await SeventhChord.create();
 
-    console.log('Data successfully uploaded!');
-    process.exit(0);
-  } catch (err) {
-      throw err;
-  }
+        console.log('Data successfully uploaded!');
+        process.exit(0);
+    } catch (err) {
+        throw err;
+    }
 });
