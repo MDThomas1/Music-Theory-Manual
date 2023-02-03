feb3c5de06efb0316b4ace71a221e8584f1b4210
Miles Thomas <milesman2013@gmail.com>
2023-02-03 17:42:34 +1100

    added new files to seeder

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,45 +1,85 @@
 const db = require('../config/connection');
 
 const { 
-  User, 
-  Chord, 
-  Interval, 
-  Rhythm, 
-  Scale, 
-  Symbol
+    User, 
+    Clef, 
+    Accidental, 
+    KeySignature, 
+    Rhythm, 
+    Rest, 
+    Triplet, 
+    PerfectInterval, 
+    MajorInterval, 
+    MinorInterval, 
+    MajorMode, 
+    Pentatonic, 
+    OtherScale, 
+    Triad, 
+    SusChord, 
+    SixthChord, 
+    SeventhChord 
 } = require('../models');
 
 const userSeeds = require('./userSeeds.json');
 
-const chordSeeds = require('./chordSeeds.json');
-const intervalSeeds = require('./intervalSeeds.json');
 const rhythmSeeds = require('./rhythmSeeds.json');
-const scaleSeeds = require('./scaleSeeds.json')
-const symbolSeeds = require('./symbolSeeds.json')
 
 db.once('open', async () => {
   try {
     await User.deleteMany({});
     await User.create(userSeeds);
 
-    await Chord.deleteMany({})
-    await Chord.create(chordSeeds)
+    await Clef.deleteMany({});
+    await Clef.create();
 
-    await Interval.deleteMany({})
-    await Interval.create(intervalSeeds)
+    await Accidental.deleteMany({});
+    await Accidental.create();
 
-    await Rhythm.deleteMany({})
-    await Rhythm.create(rhythmSeeds)
+    await KeySignature.deleteMany({});
+    await KeySignature.create();
 
-    await Scale.deleteMany({})
-    await Scale.create(scaleSeeds)
+    await Rhythm.deleteMany({});
+    await Rhythm.create(rhythmSeeds);
 
-    await Symbol.deleteMany({})
-    await Symbol.create(symbolSeeds)
+    await Rest.deleteMany({});
+    await Rest.create();
+
+    await Triplet.deleteMany({});
+    await Triplet.create();
+
+    await PerfectInterval.deleteMany({});
+    await PerfectInterval.create();
+
+    await MajorInterval.deleteMany({});
+    await MajorInterval.create();
+
+    await MinorInterval.deleteMany({});
+    await MinorInterval.create();
+
+    await MajorMode.deleteMany({});
+    await MajorMode.create();
+
+    await Pentatonic.deleteMany({});
+    await Pentatonic.create();
+
+    await OtherScale.deleteMany({});
+    await OtherScale.create();
+
+    await Triad.deleteMany({});
+    await Triad.create();
+
+    await SusChord.deleteMany({});
+    await SusChord.create();
+
+    await SixthChord.deleteMany({});
+    await SixthChord.create();
+
+    await SeventhChord.deleteMany({});
+    await SeventhChord.create();
 
     console.log('Data successfully uploaded!');
     process.exit(0);
   } catch (err) {
-    throw err;
+      throw err;
   }
 });
