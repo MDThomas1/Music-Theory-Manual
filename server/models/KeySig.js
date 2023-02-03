b7f36442029cccbffa020fe5d3b65d69d3de89e5
Miles Thomas <milesman2013@gmail.com>
2023-02-03 17:18:13 +1100

    created new symbol models

---
+++ server/models/KeySig.js
@@ -0,0 +1,24 @@
+const { Schema, model } = require('mongoose');
+
+const keySigSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    subsection: {
+        type: String,
+        required: true
+    }
+})
+
+const KeySignature = model('KeySignature', keySigSchema)
+
+module.exports = KeySignature
