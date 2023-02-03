bf68ea68a080f323384e986748f4ccf7745e6b74
Miles Thomas <milesman2013@gmail.com>
2023-02-03 16:43:58 +1100

    added new chord models

---
+++ server/models/SeventhChord.js
@@ -0,0 +1,28 @@
+const { Schema, model } = require('mongoose');
+
+const seventhChordSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    structure: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    subsection: {
+        type: String,
+        required: true
+    }
+})
+
+const SeventhChord = model('SeventhChord', seventhChordSchema)
+
+module.exports = SeventhChord
---
+++ server/models/SixthChord.js
@@ -0,0 +1,28 @@
+const { Schema, model } = require('mongoose');
+
+const sixthChordSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    structure: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    subsection: {
+        type: String,
+        required: true
+    }
+})
+
+const SixthChord = model('SixthCord', sixthChordSchema)
+
+module.exports = SixthChord
