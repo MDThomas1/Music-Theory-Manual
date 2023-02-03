7185d8551f40312b5f490e142df10a866ca2c4a0
Miles Thomas <milesman2013@gmail.com>
2023-02-03 18:02:57 +1100

    new model file for rhythm addition

---
+++ server/models/RhythmAddition.js
@@ -0,0 +1,24 @@
+const { Schema, model } = require('mongoose');
+
+const rhythmAdditionSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    subsection: {
+        type: String,
+        required: true
+    }
+})
+
+const RhythmAddition = model('RhythmAddition', rhythmAdditionSchema)
+
+module.exports = RhythmAddition
