f208d2df460276143c83c25dacd6fc1abecfa119
Miles Thomas <milesman2013@gmail.com>
2023-02-05 01:28:41 +1100

    moved css to index

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,6 @@ import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@ap
 import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import './reset.css'
-import './style.css'
-
 import Header from './components/Header';
 import Navbar from './components/Navbar';
 
