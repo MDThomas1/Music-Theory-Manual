47d5bdca0d17864dec5b6bbdeb6536aae405a5d6
Miles Thomas <milesman2013@gmail.com>
2023-02-06 02:01:07 +1100

    trying to remove the iframe

--- client/src/index.js
+++ client/src/index.js
@@ -4,8 +4,8 @@ import './reset.css'
 import './style.css'
 import App from './App';
 
-const root = ReactDOM.createRoot(
-    document.getElementById('root')
-  );
-  const element = <React.StrictMode><App /></React.StrictMode>;
-  root.render(element);
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
